2e85fabda50201cec9be2905b8716ac7120f5fb2
P1514 <guinunogois@gmail.com>
2017-09-25 11:28:05 +0100

    Software Setup

---
+++ src/login/redirect.java
@@ -0,0 +1,35 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class redirect extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		if (request.isUserInRole("admin")) {
+			response.sendRedirect("admin");
+			return;
+		}
+		if (request.isUserInRole("pai")) {
+			response.sendRedirect("pai");
+			return;
+		}
+
+		try {
+			request.logout();
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		response.sendRedirect("pages?error=2");
+
+	}
+
+}
